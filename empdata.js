c8e076f559f457a6bd9be759c45c8f9127065021
keith <castelinokei@seattleu.edu>
2019-04-19 18:38:20 -0700

    change column names

--- empdata.js
+++ empdata.js
@@ -2,123 +2,132 @@
   "data": [
     {
       "name": "James Bond",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "country": "Accountant",
+      "previous_club": "$170,750",
+      "position": "2011/07/25",
+      "date_of_birth": "Tokyo",
+      "age": "8422"
     },
     {
       "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
+      "country": "Integration Specialist",
+      "previous_club": "$372,000",
+      "position": "2012/12/02",
+      "date_of_birth": "New York",
+      "age": "4804"
     },
     {
       "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
+      "country": "Junior Technical Author",
+      "previous_club": "$86,000",
+      "position": "2009/01/12",
+      "date_of_birth": "San Francisco",
+      "age": "1562"
     },
     {
       "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
+      "country": "Accountant",
+      "previous_club": "$162,700",
+      "position": "2008/11/28",
+      "date_of_birth": "Tokyo",
+      "age": "5407"
     },
     {
       "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
+      "country": "Pre-Sales Support",
+      "previous_club": "$106,450",
+      "position": "2011/12/12",
+      "date_of_birth": "New York",
+      "age": "8330"
     },
     {
       "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
+      "country": "Regional Director",
+      "previous_club": "$183,000",
+      "position": "2008/11/13",
+      "date_of_birth": "Edinburgh",
+      "age": "6373"
     },
     {
       "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
+      "country": "Senior Javascript Developer",
+      "previous_club": "$433,060",
+      "position": "2012/03/29",
+      "date_of_birth": "Edinburgh",
+      "age": "6224"
     },
     {
       "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
+      "country": "Senior Marketing Designer",
+      "previous_club": "$313,500",
+      "position": "2012/12/18",
+      "date_of_birth": "London",
+      "age": "3597"
     },
     {
       "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
+      "country": "Javascript Developer",
+      "previous_club": "$205,500",
+      "position": "2009/09/15",
+      "date_of_birth": "San Francisco",
+      "age": "2360"
     },
     {
       "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
+      "country": "Personnel Lead",
+      "previous_club": "$217,500",
+      "position": "2012/09/26",
+      "date_of_birth": "Edinburgh",
+      "age": "2290"
     },
     {
       "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
+      "country": "Sales Assistant",
+      "previous_club": "$137,500",
+      "position": "2012/08/06",
+      "date_of_birth": "San Francisco",
+      "age": "9608"
     },
     {
       "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
+      "country": "Integration Specialist",
+      "previous_club": "$327,900",
+      "position": "2010/10/14",
+      "date_of_birth": "Tokyo",
+      "age": "6200"
     },
     {
       "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
+      "country": "Software Engineer",
+      "previous_club": "$103,600",
+      "position": "2008/12/13",
+      "date_of_birth": "Edinburgh",
+      "age": "1667"
     },
     {
       "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
+      "country": "date_of_birth Manager",
+      "previous_club": "$90,560",
+      "position": "2008/12/19",
+      "date_of_birth": "London",
+      "age": "3814"
     },
     {
       "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
+      "country": "Support Lead",
+      "previous_club": "$342,000",
+      "position": "2013/03/03",
+      "date_of_birth": "Edinburgh",
+      "age": "9497"
     }
   ]
 }
+
+{		
+	name:	James Bond,
+	country:	Accountant,
+	previous_club:	$170,750,
+	position:	2011/07/25,
+	date_of_birth:	Tokyo,
+	age:	8422
+	}	
