70b3e33b84200fae94400221acdd17495299b543
keith <castelinokei@seattleu.edu>
2019-04-19 19:06:38 -0700

    added missing commas in json

--- empdata.js
+++ empdata.js
@@ -9,148 +9,147 @@
       "age": "8422"
     },
     {
-      "name": "Alisson Becker"
-      "country": "Brazil"
-      "previous_club": "AS Roma"
-      "position": "Goalkeeper"
-      "date_of_birth": "10/2/1992""
+      "name": "Alisson Becker",
+      "country": "Brazil",
+      "previous_club": "AS Roma",
+      "position": "Goalkeeper",
+      "date_of_birth": "10/2/1992",
       "age": "26"
-
     },
     {
-      "name": "Trent Alexander-Arnold"
-      "country": "England"
-      "previous_club": "Liverpool Academy"
-      "position": "Defender"
-      "date_of_birth": "10/7/1998""
+      "name": "Trent Alexander-Arnold",
+      "country": "England",
+      "previous_club": "Liverpool Academy",
+      "position": "Defender",
+      "date_of_birth": "10/7/1998",
       "age": "20"
     },
     {
-      "name": "Joe Gomez"
-      "country": "England"
-      "previous_club": "Charlton Athletic"
-      "position": "Defender"
-      "date_of_birth": "5/23/1997""
+      "name": "Joe Gomez",
+      "country": "England",
+      "previous_club": "Charlton Athletic",
+      "position": "Defender",
+      "date_of_birth": "5/23/1997",
       "age": "21"
     },
     {
-      "name": "Virgil van Dijk"
-      "country": "The Netherlands"
-      "previous_club": "Southampton"
-      "position": "Defender"
-      "date_of_birth": "7/8/1991""
+      "name": "Virgil van Dijk",
+      "country": "The Netherlands",
+      "previous_club": "Southampton",
+      "position": "Defender",
+      "date_of_birth": "7/8/1991",
       "age": "27"
     },
     {
-      "name": "Andrew Robertson"
-      "country": "Scotland"
-      "previous_club": "Hull City"
-      "position": "Defender"
-      "date_of_birth": "3/11/1994""
+      "name": "Andrew Robertson",
+      "country": "Scotland",
+      "previous_club": "Hull City",
+      "position": "Defender",
+      "date_of_birth": "3/11/1994",
       "age": "25"
     },
     {
-      "name": "Fabinho Tavares"
-      "country": "Brazil"
-      "previous_club": "AS Monaco"
-      "position": "Midfielder"
-      "date_of_birth": "10/23/1993""
+      "name": "Fabinho Tavares",
+      "country": "Brazil",
+      "previous_club": "AS Monaco",
+      "position": "Midfielder",
+      "date_of_birth": "10/23/1993",
       "age": "25"
     },
     {
-      "name": "Jordan Henderson"
-      "country": "England"
-      "previous_club": "Sunderland"
-      "position": "Midfielder"
-      "date_of_birth": "6/17/1990""
+      "name": "Jordan Henderson",
+      "country": "England",
+      "previous_club": "Sunderland",
+      "position": "Midfielder",
+      "date_of_birth": "6/17/1990",
       "age": "28"
     },
     {
-      "name": "Naby Keita"
-      "country": "Guinea"
-      "previous_club": "RB Leipzig"
-      "position": "Midfielder"
-      "date_of_birth": "2/10/1995""
+      "name": "Naby Keita",
+      "country": "Guinea",
+      "previous_club": "RB Leipzig",
+      "position": "Midfielder",
+      "date_of_birth": "2/10/1995",
       "age": "24"
     },
     {
-      "name": "Sadio Mane"
-      "country": "Senegal"
-      "previous_club": "Southampton"
-      "position": "Forward"
-      "date_of_birth": "4/10/1992""
+      "name": "Sadio Mane",
+      "country": "Senegal",
+      "previous_club": "Southampton",
+      "position": "Forward",
+      "date_of_birth": "4/10/1992",
       "age": "27"
     },
     {
-      "name": "Mohamed Salah"
-      "country": "Egypt"
-      "previous_club": "AS Roma"
-      "position": "Forward"
-      "date_of_birth": "6/15/1992""
+      "name": "Mohamed Salah",
+      "country": "Egypt",
+      "previous_club": "AS Roma",
+      "position": "Forward",
+      "date_of_birth": "6/15/1992",
       "age": "26"
     },
     {
-      "name": "Roberto Firmino"
-      "country": "Brazil"
-      "previous_club": "Hoffenheim"
-      "position": "Forward"
-      "date_of_birth": "8/15/1993""
+      "name": "Roberto Firmino",
+      "country": "Brazil",
+      "previous_club": "Hoffenheim",
+      "position": "Forward",
+      "date_of_birth": "8/15/1993",
       "age": "25"
     },
     {
-      "name": "Simon Mignolet"
-      "country": "Belgium"
-      "previous_club": "Sunderland"
-      "position": "Goalkeeper"
-      "date_of_birth": "3/6/1988""
+      "name": "Simon Mignolet",
+      "country": "Belgium",
+      "previous_club": "Sunderland",
+      "position": "Goalkeeper",
+      "date_of_birth": "3/6/1988",
       "age": "31"
     },
     {
-      "name": "James Milner"
-      "country": "England"
-      "previous_club": "Manchester City"
-      "position": "Midfielder"
-      "date_of_birth": "1/4/1986""
+      "name": "James Milner",
+      "country": "England",
+      "previous_club": "Manchester City",
+      "position": "Midfielder",
+      "date_of_birth": "1/4/1986",
       "age": "33"
     },
     {
-      "name": "Daniel Sturridge"
-      "country": "England"
-      "previous_club": "Chelsea"
-      "position": "Forward"
-      "date_of_birth": "9/1/1989""
+      "name": "Daniel Sturridge",
+      "country": "England",
+      "previous_club": "Chelsea",
+      "position": "Forward",
+      "date_of_birth": "9/1/1989",
       "age": "29"
     },
     {
-      "name": "Xherdan Shaqiri"
-      "country": "Switzerland"
-      "previous_club": "Stoke City"
-      "position": "Forward"
-      "date_of_birth": "10/10/1991""
+      "name": "Xherdan Shaqiri",
+      "country": "Switzerland",
+      "previous_club": "Stoke City",
+      "position": "Forward",
+      "date_of_birth": "10/10/1991",
       "age": "27"
     },
     {
-      "name": "Divock Origi"
-      "country": "Belgium"
-      "previous_club": "Lille"
-      "position": "Forward"
-      "date_of_birth": "4/18/1995""
+      "name": "Divock Origi",
+      "country": "Belgium",
+      "previous_club": "Lille",
+      "position": "Forward",
+      "date_of_birth": "4/18/1995",
       "age": "24"
     },
     {
-      "name": "Alex Oxlade-Chamberlain"
-      "country": "England"
-      "previous_club": "Arsenal"
-      "position": "Midfielder"
-      "date_of_birth": "8/15/1993""
+      "name": "Alex Oxlade-Chamberlain",
+      "country": "England",
+      "previous_club": "Arsenal",
+      "position": "Midfielder",
+      "date_of_birth": "8/15/1993",
       "age": "25"
     },
     {
-      "name": "Gini Wijnaldum"
-      "country": "The Netherlands"
-      "previous_club": "Newcastle United"
-      "position": "Midfielder"
-      "date_of_birth": "11/11/1990""
+      "name": "Gini Wijnaldum",
+      "country": "The Netherlands",
+      "previous_club": "Newcastle United",
+      "position": "Midfielder",
+      "date_of_birth": "11/11/1990",
       "age": "28"
     }
   ]
